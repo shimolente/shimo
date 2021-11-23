b5fb3753ac22329814bd4f9a58f2ace8de712dcc
nyomanjyotisa <jyotisa1616@gmail.com>
2021-11-23 20:52:16 +0800

    update dikit di xml

--- app/src/main/java/id/shimo/mindfulness/AddJurnalActivity.java
+++ app/src/main/java/id/shimo/mindfulness/AddJurnalActivity.java
@@ -1,20 +1,46 @@
 package id.shimo.mindfulness;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.SeekBar;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.textfield.TextInputLayout;
 
+import java.util.ArrayList;
+
 public class AddJurnalActivity extends AppCompatActivity {
 
-    private TextInputLayout etCompanyName, etJobTitle, etJobDesc, etCountry;
-    private SeekBar seekBarSalary;
+    private TextInputLayout etBestThing, etWorstThing;
+    private SeekBar seekRate;
+    private ArrayList<String> didChecks;
+    private CheckBox smileCheck, gratefulCheck, careCheck;
+    private Button btnSubmit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.add_jurnal);
+
+        etBestThing = findViewById(R.id.bestThing);
+        etWorstThing = findViewById(R.id.worstThing);
+        seekRate = findViewById(R.id.rateSeek);
+
+        smileCheck = (CheckBox)findViewById(R.id.smileCheck);
+        gratefulCheck = (CheckBox)findViewById(R.id.gratefulCheck);
+        careCheck = (CheckBox)findViewById(R.id.careCheck);
+        didChecks = new ArrayList<>();
+
+        btnSubmit = (Button) findViewById(R.id.button);
+        btnSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
