15536c07be765142e7f86ada1421c79d0fc1dbc9
dekpas <pastika.rahayu01@gmail.com>
2021-11-23 20:13:41 +0800

    mod1sample

---
+++ app/src/main/java/id/shimo/mindfulness/AddJurnalActivity.java
@@ -0,0 +1,15 @@
+package id.shimo.mindfulness;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AddJurnalActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.add_jurnal);
+    }
+}
