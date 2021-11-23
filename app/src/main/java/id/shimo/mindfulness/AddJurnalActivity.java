71491787a76644958839cabf8c8a65b0b8351145
nyomanjyotisa <jyotisa1616@gmail.com>
2021-11-23 20:41:36 +0800

    ada salah dikit

--- app/src/main/java/id/shimo/mindfulness/AddJurnalActivity.java
+++ app/src/main/java/id/shimo/mindfulness/AddJurnalActivity.java
@@ -1,11 +1,16 @@
 package id.shimo.mindfulness;
 
 import android.os.Bundle;
+import android.widget.SeekBar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.material.textfield.TextInputLayout;
+
 public class AddJurnalActivity extends AppCompatActivity {
 
+    private TextInputLayout etCompanyName, etJobTitle, etJobDesc, etCountry;
+    private SeekBar seekBarSalary;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
